3b061938ff30eadc0e93e8821a328f64c9407220
ayham@gmail.com <152860926+Ayham1najeb@users.noreply.github.com>
2025-12-02 20:36:25 +0300

    Fix CSP to allow media data URIs for audio notifications

--- app/Http/Middleware/SecurityHeaders.php
+++ app/Http/Middleware/SecurityHeaders.php
@@ -22,7 +22,7 @@ class SecurityHeaders
         $response->headers->set('X-XSS-Protection', '1; mode=block');
         $response->headers->set('Strict-Transport-Security', 'max-age=31536000; includeSubDomains');
         $response->headers->set('Referrer-Policy', 'no-referrer-when-downgrade');
-        $response->headers->set('Content-Security-Policy', "default-src 'self'; script-src 'self' 'unsafe-inline' 'unsafe-eval'; style-src 'self' 'unsafe-inline'; img-src 'self' data: https:; font-src 'self' data:; connect-src 'self';");
+        $response->headers->set('Content-Security-Policy', "default-src 'self'; script-src 'self' 'unsafe-inline' 'unsafe-eval'; style-src 'self' 'unsafe-inline'; img-src 'self' data: https:; font-src 'self' data:; media-src 'self' data: https:; connect-src 'self';");
 
         return $response;
     }
